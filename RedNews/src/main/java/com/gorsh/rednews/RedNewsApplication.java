4c6ccdd5c17c0f024329b13b49539d3dd2991b57
asgor <alexmilesjg@gmail.com>
2022-09-29 14:46:45 +0500

    init

---
+++ RedNews/src/main/java/com/gorsh/rednews/RedNewsApplication.java
@@ -0,0 +1,13 @@
+package com.gorsh.rednews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedNewsApplication.class, args);
+    }
+
+}
